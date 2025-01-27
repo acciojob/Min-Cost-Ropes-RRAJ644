c634b2ea8ea301dceb5da66d38f53ec3413e83f2
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2025-01-27 22:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	const n = arr.length 
+	let totalCost = 0
+	while (n>1) {
+		arr.sort((a,b)=>a-b)
+		const first = arr.shift()
+		const second = arr.shift()
+		const cost = first+second
+		totalCost+=const
+		arr.push(cost)
+		
+	}
+
+	return totalCost
   
 }
 
